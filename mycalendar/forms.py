da5e2b392a05b8b91c13df37e4cfcd0a9c614e10
jan <janlemcke@live.de>
2021-03-31 12:36:47 +0200

    home Seite aktualisiert.

--- mycalendar/forms.py
+++ mycalendar/forms.py
@@ -3,13 +3,14 @@ from django import forms
 from mycalendar.models import Calendar, Event
 from account.models import Account
 
+
 class CalendarForm(forms.ModelForm):
     visible_for = forms.CharField()
     editable_by = forms.CharField()
 
     class Meta:
         model = Calendar
-        exclude = ("owner","visible_for","editable_by")
+        exclude = ("owner", "visible_for", "editable_by")
 
     def save(self, commit=True):
         calendar = self.instance
@@ -22,6 +23,25 @@ class CalendarForm(forms.ModelForm):
         if commit:
             calendar.save()
         return calendar
+
+
+def get_calendars(user_id):
+    calendars = Calendar.objects.filter(owner=user_id)
+    print(calendars)
+    choices = []
+
+    for calendar in calendars:
+        choices.append((calendar.pk, calendar.name))
+
+    return choices
+
+
+class CalendarEditForm(CalendarForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CalendarEditForm, self).__init__(*args, **kwargs)
+        self.fields["calendars"] = forms.ChoiceField(choices=get_calendars(self.initial["user_id"]), required=True)
+
 
 class EventForm(forms.ModelForm):
     class Meta:
--- mycalendar/views.py
+++ mycalendar/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from mycalendar.forms import CalendarForm
+from mycalendar.forms import CalendarForm, CalendarEditForm
 from mycalendar.models import Calendar, Event
 
 @login_required
@@ -9,16 +9,25 @@ def homeView(request):
     context = {}
 
     if request.POST:
-        form = CalendarForm(request.POST)
-        if form.is_valid():
-            calendar = form.save(commit=False)
-            calendar.owner_id = request.user.pk
-            calendar.save()
-            print(calendar)
+        if request.POST['action'] == 'create':
+            form = CalendarForm(request.POST)
+            if form.is_valid():
+                calendar = form.save(commit=False)
+                calendar.owner_id = request.user.pk
+                calendar.save()
+
+        if request.POST['action'] == 'edit':
+            form = CalendarEditForm(request.POST)
+            if form.is_valid():
+                calendar = form.save(commit=False)
+                calendar.owner_id = request.user.pk
+                calendar.save()
 
     calendars = Calendar.objects.filter(owner=request.user)
 
-    form = CalendarForm()
-    context["form"] = form
+    createform = CalendarForm()
+    editform = CalendarEditForm(initial={"user_id": request.user.pk})
+    context["createform"] = createform
+    context["editform"] = editform
 
     return render(request, "home.html", context)
